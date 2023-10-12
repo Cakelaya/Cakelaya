4f5c4282641c42cc91be8f82bbd51244d1608998
Vaibhav Pathak <vaibhavpathak852947@gmail.com>
2023-10-13 00:35:57 +0530

    Update navbar with more

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -16,7 +16,7 @@ import { Link, useHistory, useLocation } from "react-router-dom";
 import { logOut } from "../redux/userRedux";
 import { placeCity } from "../redux/cityRedux";
 import { useEffect } from "react";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 import { useRef } from "react";
 import { removeProduct } from "../redux/cartRedux";
 
@@ -24,7 +24,7 @@ const Filter = styled.div`
   margin: 20px;
   display: flex;
   justify-content: center;
-    align-items: center;
+  align-items: center;
   gap: 0.5rem;
   cursor: pointer;
   @media (max-width: 820px) {
@@ -45,58 +45,57 @@ const FilterText = styled.span`
 `;
 
 const City = styled.ul`
-    list-style: none;
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
-    margin-top: 40px;
-    width: 100%;
-    justify-content: space-around;
-    padding: 0px;
-    button{
-      border: 2px solid black;
-      padding: 5px;
-      width: 100px;
-      text-align: center;
-      background: none;
-      font-size: 15px;
-      cursor: pointer;
-    }
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  margin-top: 40px;
+  width: 100%;
+  justify-content: space-around;
+  padding: 0px;
+  button {
+    border: 2px solid black;
+    padding: 5px;
+    width: 100px;
+    text-align: center;
+    background: none;
+    font-size: 15px;
+    cursor: pointer;
+  }
 `;
 
 const Pop = styled.div`
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 100%;
-    z-index: 500;
-    background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 500;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-flow: row nowrap;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  div {
+    background-color: rgb(255, 255, 255);
+    opacity: 1;
+    width: auto;
+    height: 20%;
     display: flex;
-    flex-flow: row nowrap;
-    -webkit-box-align: center;
-    align-items: center;
-    -webkit-box-pack: center;
     justify-content: center;
-    div{
-      background-color: rgb(255, 255, 255);
-      opacity: 1;
+    align-items: center;
+    position: absolute;
+    padding: 0px;
+    flex-direction: column;
+    border: 2px solid lightgray;
+    padding: 0px 50px;
+    span {
       width: auto;
-      height: 20%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      position: absolute;
-      padding: 0px;
-      flex-direction: column;
-      border: 2px solid lightgray;
-      padding: 0px 50px;
-      span{
-        width: auto;
-        margin: 0px auto;
-      }
+      margin: 0px auto;
     }
-
+  }
 `;
 
 const Left = styled.a`
@@ -106,7 +105,7 @@ const Left = styled.a`
   color: black;
   text-decoration: none;
   width: 168px;
-    margin-left: 10px;
+  margin-left: 10px;
 
   img {
     width: 100%;
@@ -122,9 +121,24 @@ const MenuItem = styled.a`
   z-index: 1000;
 `;
 
+const List = styled.ul`
+  z-index: 1000;
+  top: 4.5vh;
+  background-color: #fff;
+  position: absolute;
+  height: 116px;
+  width: 128px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 0px;
+  border-radius: 10px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2);
+`;
+
 export default function Header() {
   // on small screens, display nav on burger icon
-
 
   const quantity = useSelector((state) => state.cart.quantity);
   const city = useSelector((state) => state.city.city);
@@ -135,20 +149,20 @@ export default function Header() {
   const [navStatus, setNavStatus] = useState("navbar-container");
   const location = useLocation();
   const history = useHistory();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleNav = () => {
     if (navStatus === "navbar-container") {
-      setNavStatus('active');
+      setNavStatus("active");
       setIcon(true);
-    }
-    else {
-      setNavStatus('navbar-container');
+    } else {
+      setNavStatus("navbar-container");
       setIcon(false);
     }
   };
 
   // display dropdown menu for feature/company icon
-  const handleDropdown = e => {
+  const handleDropdown = (e) => {
     const features = document.querySelector(".features");
     const company = document.querySelector(".company");
     const iconFeatures = document.querySelector(".icon-features");
@@ -167,16 +181,14 @@ export default function Header() {
     const temp = e.target.innerHTML;
     window.location.href = "https://cakelaya.com";
     dispatch(removeProduct({}));
-    dispatch(
-      placeCity({ temp })
-    );
+    dispatch(placeCity({ temp }));
     setState(temp);
-  }
+  };
 
   const handleChange = () => {
     setState(undefined);
     document.body.style.overflow = "hidden";
-  }
+  };
 
   const handleClick = () => {
     dispatch(logOut());
@@ -191,12 +203,12 @@ export default function Header() {
 
   const handleClickOutside = (e) => {
     if (!refOne?.current?.contains(e.target)) {
-      if (navStatus === 'active') {
-        setNavStatus('navbar-container');
+      if (navStatus === "active") {
+        setNavStatus("navbar-container");
         setIcon(true);
       }
     }
-  }
+  };
 
   var body = document.body,
     html = document.documentElement;
@@ -224,7 +236,7 @@ export default function Header() {
 
   return (
     <StyledHeader>
-      {!state &&
+      {!state && (
         <Pop>
           <Filter>
             <FilterText>Select Your City</FilterText>
@@ -234,69 +246,85 @@ export default function Header() {
             </City>
           </Filter>
         </Pop>
-      }
+      )}
       <div style={{ display: "flex" }}>
-        <Left href={(location.pathname === '/gift' || location?.state?.prevPath === '/gift') ? '/gift' : '/'}>
-          {state === "Sultanpur" && (location.pathname !== '/gift' && location?.state?.prevPath !== '/gift')
-            &&
+        <Left
+          href={
+            location.pathname === "/gift" ||
+            location?.state?.prevPath === "/gift"
+              ? "/gift"
+              : "/"
+          }
+        >
+          {state === "Sultanpur" &&
+            location.pathname !== "/gift" &&
+            location?.state?.prevPath !== "/gift" && (
+              <img
+                src="/images/logoS.png"
+                alt=""
+                style={{ marginTop: "-4px" }}
+              />
+            )}
+          {state === "Noida" &&
+            location.pathname !== "/gift" &&
+            location?.state?.prevPath !== "/gift" && (
+              <img
+                src="/images/logoN.png"
+                alt=""
+                style={{ marginTop: "-4px" }}
+              />
+            )}
+          {!state &&
+            location.pathname !== "/gift" &&
+            location?.state?.prevPath !== "/gift" && (
+              <img
+                src="/images/Mainlogo.png"
+                alt=""
+                style={{ marginTop: "-4px" }}
+              />
+            )}
+          {(location.pathname === "/gift" ||
+            location?.state?.prevPath === "/gift") && (
             <img
-              src="/images/logoS.png"
+              src="/images/Gifting.png"
               alt=""
-              style={{ "marginTop": "-4px" }}
+              style={{ marginTop: "-4px" }}
             />
-          }
-          {state === "Noida" && (location.pathname !== '/gift' && location?.state?.prevPath !== '/gift')
-            &&
-            <img
-              src="/images/logoN.png"
-              alt=""
-              style={{ "marginTop": "-4px" }}
-            />
-          }
-          {!state && (location.pathname !== '/gift' && location?.state?.prevPath !== '/gift')
-            &&
-            <img
-              src="/images/Mainlogo.png"
-              alt=""
-              style={{ "marginTop": "-4px" }}
-            />
-          }
-          {
-            (location.pathname === '/gift' || location?.state?.prevPath === '/gift') && <img src="/images/Gifting.png"
-              alt=""
-              style={{ "marginTop": "-4px" }} />
-          }
+          )}
         </Left>
-        {(location.pathname !== '/gift' && location?.state?.prevPath !== '/gift') && <Filter onClick={handleChange}>
-          <Icon icon="ic:baseline-location-on" width="28" height="28" />
-          <img
-            className="icon-features"
-            src="/images/icon-arrow-down.svg"
-            alt=""
-          />
-        </Filter>}
+        {location.pathname !== "/gift" &&
+          location?.state?.prevPath !== "/gift" && (
+            <Filter onClick={handleChange}>
+              <Icon icon="ic:baseline-location-on" width="28" height="28" />
+              <img
+                className="icon-features"
+                src="/images/icon-arrow-down.svg"
+                alt=""
+              />
+            </Filter>
+          )}
       </div>
       <StyledNav>
         <StyledNavBar className={navStatus}>
           <ul className="navbar" ref={refOne}>
             <li>
-              {(location.pathname !== '/gift' && location?.state?.prevPath !== '/gift') && <a
-                className="link-features"
-                href="/gift"
-              >
-                Gift Engine
-              </a>}
-              {(location.pathname === '/gift' || location?.state?.prevPath === '/gift') && <a
-                className="link-features"
-                href="/"
-              >
-                Home
-              </a>}
+              {location.pathname !== "/gift" &&
+                location?.state?.prevPath !== "/gift" && (
+                  <a className="link-features" href="/gift">
+                    Gift Engine
+                  </a>
+                )}
+              {(location.pathname === "/gift" ||
+                location?.state?.prevPath === "/gift") && (
+                <a className="link-features" href="/">
+                  Home
+                </a>
+              )}
             </li>
             <li>
               <a
                 className="link-company"
-                onClick={e => handleDropdown(e)}
+                onClick={(e) => handleDropdown(e)}
                 style={{ cursor: "pointer" }}
               >
                 Account
@@ -308,43 +336,105 @@ export default function Header() {
               </a>
               <StyledDropdown className="dropdown company">
                 <ul className="dropdown-menu">
-                  <MenuItem onClick={() => {
-                    history.push({ pathname: '/myorders', state: { prevPath: location.pathname } })
-                  }}>My Orders</MenuItem>
-                  {admin && <MenuItem onClick={() => {
-                    history.push({ pathname: '/headq', state: { prevPath: location.pathname } })
-                  }}>Admin</MenuItem>}
-                  {merchant && <MenuItem onClick={() => {
-                    history.push({ pathname: '/inventory', state: { prevPath: location.pathname } })
-                  }}>Admin</MenuItem>}
+                  <MenuItem
+                    onClick={() => {
+                      history.push({
+                        pathname: "/myorders",
+                        state: { prevPath: location.pathname },
+                      });
+                    }}
+                  >
+                    My Orders
+                  </MenuItem>
+                  {admin && (
+                    <MenuItem
+                      onClick={() => {
+                        history.push({
+                          pathname: "/headq",
+                          state: { prevPath: location.pathname },
+                        });
+                      }}
+                    >
+                      Admin
+                    </MenuItem>
+                  )}
+                  {merchant && (
+                    <MenuItem
+                      onClick={() => {
+                        history.push({
+                          pathname: "/inventory",
+                          state: { prevPath: location.pathname },
+                        });
+                      }}
+                    >
+                      Admin
+                    </MenuItem>
+                  )}
                   {user && <MenuItem onClick={handleClick}>Sign Out</MenuItem>}
                   {!user && <MenuItem href="/login">Sign In</MenuItem>}
                 </ul>
               </StyledDropdown>
             </li>
-            <li>
-              <p style={{ cursor: 'pointer' }} onClick={() => {
-                history.push({ pathname: '/about', state: { prevPath: location.pathname } })
-              }}>About</p>
+            <li style={{ position: "relative" }}>
+              <a
+                onClick={() => setIsOpen(!isOpen)}
+                style={{ cursor: "pointer" }}
+              >
+                More
+                {!isOpen ? (
+                  <img src="/images/icon-arrow-down.svg" alt="" />
+                ) : (
+                  <img src="/images/icon-arrow-up.svg" />
+                )}
+              </a>
+              <List style={{ display: isOpen ? "flex" : "none" }}>
+                <li>
+                  <MenuItem
+                    onClick={() => {
+                      history.push({
+                        pathname: "/about",
+                        state: { prevPath: location.pathname },
+                      });
+                    }}
+                  >
+                    About Us
+                  </MenuItem>
+                </li>
+                <li>
+                  <MenuItem
+                    onClick={() => {
+                      history.push({
+                        pathname: "/privacy",
+                        state: { prevPath: location.pathname },
+                      });
+                    }}
+                  >
+                    Privacy Policy
+                  </MenuItem>
+                </li>
+              </List>
             </li>
             <li>
-              <a onClick={goToTop} style={{ cursor: "pointer" }}>Contact Us</a>
+              <a onClick={goToTop} style={{ cursor: "pointer" }}>
+                Contact Us
+              </a>
             </li>
           </ul>
         </StyledNavBar>
-        {(location.pathname !== '/gift' && location?.state?.prevPath !== '/gift') && <Link to="/cart">
-          <MenuItem>
-            <Badge badgeContent={quantity} color="primary">
-              <ShoppingCartOutlined />
-            </Badge>
-          </MenuItem>
-        </Link>}
+        {location.pathname !== "/gift" &&
+          location?.state?.prevPath !== "/gift" && (
+            <Link to="/cart">
+              <MenuItem>
+                <Badge badgeContent={quantity} color="primary">
+                  <ShoppingCartOutlined />
+                </Badge>
+              </MenuItem>
+            </Link>
+          )}
         <StyledBurger onClick={() => handleNav()}>
           <img
             className="icon-burger"
-            src={
-              icon ? "/images/icon-menu.svg" : "/images/icon-close-menu.svg"
-            }
+            src={icon ? "/images/icon-menu.svg" : "/images/icon-close-menu.svg"}
             alt="Navigation bar"
           />
         </StyledBurger>
--- client/src/pages/Privacy.jsx
+++ client/src/pages/Privacy.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styled from "styled-components";
+import Announcement from "../components/Announcement";
 
 const Content = styled.div`
   /* border-top: 2px solid lightgray; */
